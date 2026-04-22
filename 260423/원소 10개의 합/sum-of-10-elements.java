2454a035a8cee3eb2cda86423f20a18c27faba36
GYUWON <37896874+GyuWonLee@users.noreply.github.com>
2026-04-23 03:23:03 +0900

    Title: 원소 10개의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260423/원소 10개의 합/sum-of-10-elements.java
+++ 260423/원소 10개의 합/sum-of-10-elements.java
@@ -4,10 +4,13 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int S = sc.nextInt();
-		int sum = 0;
-		for(int i =0; i< 10; i++) {
-			sum += i;
-		}
+		int val = 0;
+        int sum = 0;
+        for(int i = 0; i < 10; i++){
+            val = sc.nextInt();
+            sum += val;
+        }
+        System.out.print(sum);
 	}
 }
+
