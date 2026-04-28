aaaa79ec8cc51710ef5a3486d6adc49a5466c6aa
GYUWON <37896874+GyuWonLee@users.noreply.github.com>
2026-04-28 17:55:40 +0900

    Title: 2개 이상의 알파벳, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260428/2개 이상의 알파벳/more-than-one-alphabet.java
+++ 260428/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -2,24 +2,25 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static boolean isDifferent(String A) {
-		for (int i = 0; i < A.length(); i++) {
-			for (int j = i + 1; j < A.length(); j++) {
-				return true;
-			}
-		}
-		return false;
-	}
+    public static boolean isDifferent(String A) {
+        for (int i = 0; i < A.length(); i++) {
+            for (int j = i + 1; j < A.length(); j++) {
+                if (A.charAt(i) != A.charAt(j)) {
+                    return true; 
+                }
+            }
+        }
+        return false;
+    }
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String A = sc.next();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
 
-		if (isDifferent(A)) {
-			System.out.println("Yes");
-		} else {
-			System.out.println("No");
-
-		}
-	}
+        if (isDifferent(A)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
 }
